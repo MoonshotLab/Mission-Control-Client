7fe2d9c81aa696ceb7dbabdbecba42544b81ceb6
Joe Longstreet <joelongstreet@gmail.com>
2016-03-15 10:41:24 -0500

    Initial Commit

---
+++ Mission Control Client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mission Control Client
+//
+//  Created by Joe Longstreet on 3/15/16.
+//  Copyright © 2016 Joe Longstreet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
